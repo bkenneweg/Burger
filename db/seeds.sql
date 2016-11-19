042fa0b599c08fcbffb7644123943e89658c3b64
Bryan <kennewegbryan@gmail.com>
2016-11-18 21:44:42 -0600

    Major Changes

---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+USE burgers_db;
+
+INSERT INTO burgers (burger_name, devoured) VALUES('Chili Cheese Burger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES('Southwest Bacon Burger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES('Hawaiian Pineapple Burger', false);
